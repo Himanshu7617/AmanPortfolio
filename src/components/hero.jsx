52b74e7bd91f407d982ff95e5290b72d833b7b47
Himanshu <gghimanshu333@gmail.com>
2024-05-11 23:16:40 +0530

    added video element

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,15 +1,20 @@
 
 //hero.jsx
 
-import img from "../images/profile.png"
+// import img from "../images/profile.png"
 import Heading from "./heading";
 import SubHeading from "./subheading";
+import video from "../video/aman22.mp4";
+import Navbar from "./navBar";
 
 export default function Hero() {
     return (
         <div className="hero-container">
-            
-            <img alt="profilephoto" src={img} className="hero-img" />
+            <Navbar></Navbar>
+            <video className="homeScreen-background-video" loop autoPlay muted>
+                <source src={video} />
+            </video>
+            {/* <img alt="profilephoto" src={img} className="hero-img" /> */}
             <SubHeading text="Graphic" customClass= "hero-graphic"></SubHeading>
             <Heading text= "Designer" customClass="hero-designer"></Heading>
         </div>
--- src/screens/homeScreen.jsx
+++ src/screens/homeScreen.jsx
@@ -1,13 +1,13 @@
 //homeScreen.jsx
 
 
-import Navbar from "../components/navBar";
+
 import Hero from "../components/hero";
 
 
 export default function HomeScreen() {
     return <div>
-        <Navbar></Navbar>
+        
         <Hero></Hero>
     </div>
 }
